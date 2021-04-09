625c3e44b8c6b4b78e9a010c013e8825db40bd1e
Darryl Walker <darryljwalker@me.com>
2021-04-09 10:52:57 +0100

    Wiring framerate and wrapping my head around this unpacking bollocks

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,10 @@ use log::LevelFilter;
 
 use sdl2;
 use sdl2::event::Event;
+use sdl2::gfx::framerate::FPSManager;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::rect::Rect;
 use std::io::Write;
 
 // const CHIP8_WIDTH: usize = 64;
@@ -53,37 +55,65 @@ fn main() -> Result<(), String> {
         .build()
         .expect("could not initialize video subsystem");
 
-    let mut canvas = window
-        .into_canvas()
-        .build()
-        .expect("could not make a canvas");
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    let texture_creator = canvas.texture_creator();
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    let mut texture = texture_creator
+        .create_texture_streaming(PixelFormatEnum::RGB24, 256, 256)
+        .map_err(|e| e.to_string())?;
+    // Create a red-green gradient
+    texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+        for y in 0..256 {
+            for x in 0..256 {
+                let offset = y * pitch + x * 3;
+                buffer[offset] = x as u8;
+                buffer[offset + 1] = y as u8;
+                buffer[offset + 2] = 0;
+            }
+        }
+    })?;
+
     canvas.clear();
+    canvas.copy(&texture, None, Some(Rect::new(100, 100, 256, 256)))?;
+    canvas.copy_ex(
+        &texture,
+        None,
+        Some(Rect::new(450, 100, 256, 256)),
+        30.0,
+        None,
+        false,
+        false,
+    )?;
     canvas.present();
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
 
+    // track frames
+    // this syntax is weird but i guess i kind of like it
+
+    let mut fpsmanager: FPSManager = FPSManager::new();
+    fpsmanager.set_framerate(60u32).expect("Framerate panic");
+
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
         for event in event_pump.poll_iter() {
+            // log fps
+            let fps: i32 = fpsmanager.get_framerate();
+            log::info!("FPS: {:?}", fps);
+
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
-                } => {
-                    break 'running;
-                }
+                } => break 'running,
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
+        // Update
+        i = (i + 1) % 255;
 
-        canvas.present();
+        // Time management!
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
