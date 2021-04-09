fb3d34c248edba026147516072cbbc2055727ba4
Darryl Walker <darryljwalker@me.com>
2021-04-09 09:48:13 +0100

    Basic screen working

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
-// https://riptutorial.com/rust/example/23984/simple-gtkplus-window-with-text
+// with thanks to:
+// https://sunjay.dev/learn-game-dev/opening-a-window.html
 
-extern crate gtk;
+mod chip;
+
+use std::time::Duration;
 
 use chrono::Local;
-use env_logger::Builder;
-use gtk::prelude::*; // Import all the basic things
-use gtk::{CssProvider, CssProviderExt, Label, StyleContext, Window, WindowType};
+use env_logger;
 use log::LevelFilter;
+
+use sdl2;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 use std::io::Write;
 
-const CSS: &str = include_str!("styles/main.css");
+// const CHIP8_WIDTH: usize = 64;
+// const CHIP8_HEIGHT: usize = 32;
+// const CHIP8_RAM: usize = 4096;
 
-fn main() {
-    Builder::new()
+// const SCALE_FACTOR: u32 = 20;
+// const SCREEN_WIDTH: u32 = (CHIP8_WIDTH as u32) * SCALE_FACTOR;
+// const SCREEN_HEIGHT: u32 = (CHIP8_HEIGHT as u32) * SCALE_FACTOR;
+
+fn main() -> Result<(), String> {
+    // set up our logger prefix
+    // i guess i'll move this somewhere more sensible soon
+
+    env_logger::Builder::new()
         .format(|buf, record| {
             writeln!(
                 buf,
@@ -25,40 +40,52 @@ fn main() {
         .filter(None, LevelFilter::Info)
         .init();
 
-    if gtk::init().is_err() {
-        //Initialize Gtk before doing anything with it
-        panic!("Can't init GTK");
+    log::info!("Logger: {:?}", true);
+
+    // configure video
+
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window("rust-sdl2 demo", 800, 600)
+        .position_centered()
+        .build()
+        .expect("could not initialize video subsystem");
+
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("could not make a canvas");
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump()?;
+    let mut i = 0;
+
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    break 'running;
+                }
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
-    let window = Window::new(WindowType::Toplevel);
-
-    //Destroy window on exit
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-
-    window.set_title("CHIP");
-    window.set_default_size(320, 240);
-    gtk::WidgetExt::set_widget_name(&window, "window");
-
-    // css
-    let screen = window.get_screen().unwrap();
-    let style = CssProvider::new();
-    let _ = CssProviderExt::load_from_data(&style, CSS.as_bytes());
-    StyleContext::add_provider_for_screen(&screen, &style, 800);
-
-    let label = Label::new(Some("CHIP"));
-    label.set_halign(gtk::Align::Center);
-    label.set_valign(gtk::Align::Center);
-    // We need to name it in order to be able to use its name as a CSS label to
-    // apply CSS on it.
-    gtk::WidgetExt::set_widget_name(&label, "title");
-
-    window.add(&label);
-    window.show_all();
-
-    log::info!("Label: {:?}", label);
-
-    gtk::main();
+    Ok(())
 }
