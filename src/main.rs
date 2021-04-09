f58b944223584736a0aebcbb03ef197eba072dca
Darryl Walker <darryljwalker@me.com>
2021-04-09 12:10:39 +0100

    60 fps video baseline control

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 // with thanks to:
 // https://sunjay.dev/learn-game-dev/opening-a-window.html
+// https://docs.rs/sdl2/0.30.0/sdl2/render/struct.Canvas.html
 
 mod chip;
 
+use std::io::Write;
 use std::time::Duration;
 
 use chrono::Local;
@@ -13,9 +15,10 @@ use sdl2;
 use sdl2::event::Event;
 use sdl2::gfx::framerate::FPSManager;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::PixelFormatEnum;
+use sdl2::pixels::Color;
 use sdl2::rect::Rect;
-use std::io::Write;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 
 // const CHIP8_WIDTH: usize = 64;
 // const CHIP8_HEIGHT: usize = 32;
@@ -53,38 +56,13 @@ fn main() -> Result<(), String> {
         .window("rust-sdl2 demo", 800, 600)
         .position_centered()
         .build()
-        .expect("could not initialize video subsystem");
+        .unwrap();
 
-    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
-    let texture_creator = canvas.texture_creator();
+    let mut canvas: Canvas<Window> = window.into_canvas().present_vsync().build().unwrap();
 
-    let mut texture = texture_creator
-        .create_texture_streaming(PixelFormatEnum::RGB24, 256, 256)
-        .map_err(|e| e.to_string())?;
-    // Create a red-green gradient
-    texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
-        for y in 0..256 {
-            for x in 0..256 {
-                let offset = y * pitch + x * 3;
-                buffer[offset] = x as u8;
-                buffer[offset + 1] = y as u8;
-                buffer[offset + 2] = 0;
-            }
-        }
-    })?;
-
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    // fills the canvas with the color we set in `set_draw_color`.
     canvas.clear();
-    canvas.copy(&texture, None, Some(Rect::new(100, 100, 256, 256)))?;
-    canvas.copy_ex(
-        &texture,
-        None,
-        Some(Rect::new(450, 100, 256, 256)),
-        30.0,
-        None,
-        false,
-        false,
-    )?;
-    canvas.present();
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -96,25 +74,46 @@ fn main() -> Result<(), String> {
     fpsmanager.set_framerate(60u32).expect("Framerate panic");
 
     'running: loop {
-        for event in event_pump.poll_iter() {
-            // log fps
-            let fps: i32 = fpsmanager.get_framerate();
-            log::info!("FPS: {:?}", fps);
+        // log fps
+        let fps: i32 = fpsmanager.get_framerate();
+        log::info!("FPS: {:?}", fps);
 
+        // change the colour so we know it's working
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+
+        // change the color of our drawing with a gold-color ...
+        canvas.set_draw_color(Color::RGB(255, 210, 0));
+        // A draw a rectangle which almost fills our window with it !
+        let rectangle: Rect = Rect::new(10, 10, 400, 200);
+        canvas.fill_rect(rectangle).unwrap();
+
+        let square: Rect = Rect::new(250, 250, 200, 200);
+        canvas.fill_rect(square).unwrap();
+
+        for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
-                } => break 'running,
+                } => {
+                    break 'running;
+                }
                 _ => {}
             }
         }
-        // Update
-        i = (i + 1) % 255;
+        // Present the layers
+        // However the canvas has not been updated to the window yet,
+        // everything has been processed to an internal buffer,
+        // but if we want our buffer to be displayed on the window,
+        // we need to call `present`. We need to call this everytime
+        // we want to render a new frame on the window.
+        canvas.present();
 
-        // Time management!
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        // sleep until the next frame
+        ::std::thread::sleep(Duration::from_millis(1000 / 60));
     }
 
     Ok(())
